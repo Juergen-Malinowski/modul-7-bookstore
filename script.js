ae17c294b479d7fcc343c282a90506f22e775fce
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-09 21:18:08 +0200

    changes

--- script.js
+++ script.js
@@ -177,10 +177,14 @@ console.log(books[1]);
 console.log(books[1].comments);
 console.log(books[3].bookName + " von " + books[3].author + " aus " + books[3].publishedYear);
 
+
+
 for (let index = 0; index < books.length; index++) {
     console.log(books[index].discription);
+}
 
-
+for (let index = 0; index < books.length; index++) {
+    console.log("Kommentare zu 'Die Elfen' ..." + books[4].comments);
 }
 
 // End of test-area
