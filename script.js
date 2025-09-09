3dc79061f40c62b2df2940b17f33990d6a8ec725
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-09 19:14:22 +0200

    Definition new Object

--- script.js
+++ script.js
@@ -1,1 +1,163 @@
 // DEFINITION OBJECT-Database AND VARIABLE/CONST
+
+let books = [
+    {
+        "bookName": "Der Hobbit",
+        "author": "J.R.R. Tolkien",
+        "likes": 8754,
+        "liked": true,
+        "price": 18.95,
+        "publishedYear": 1937,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Halodri",
+                "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+            },
+            {
+                "name": "Bookworm84",
+                "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+            },
+            {
+                "name": "FantasyFanatic",
+                "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+            },
+            {
+                "name": "SciFiGuru",
+                "comment": "Eine fantastische Geschichte mit interessanten Konzepten und Charakteren."
+            },
+            {
+                "name": "NovelLover",
+                "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+            }
+        ]
+    },
+
+    {
+        "bookName": "Die Kinder Húrins",
+        "author": "J.R.R. Tolkien",
+        "likes": 3850,
+        "liked": true,
+        "price": 17.95,
+        "publishedYear": 2007,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Halodri",
+                "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+            },
+            {
+                "name": "Bookworm84",
+                "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+            },
+            {
+                "name": "FantasyFanatic",
+                "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+            },
+            {
+                "name": "SciFiGuru",
+                "comment": "Eine fantastische Geschichte mit interessanten Konzepten und Charakteren."
+            },
+            {
+                "name": "NovelLover",
+                "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+            }
+        ]
+    },
+
+    {
+        "bookName": "Das Silmarillion",
+        "author": "J.R.R. Tolkien",
+        "likes": 4190,
+        "liked": true,
+        "price": 22.95,
+        "publishedYear": 1977,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Halodri",
+                "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+            },
+            {
+                "name": "Bookworm84",
+                "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+            },
+            {
+                "name": "FantasyFanatic",
+                "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+            },
+            {
+                "name": "SciFiGuru",
+                "comment": "Eine fantastische Geschichte mit interessanten Konzepten und Charakteren."
+            },
+            {
+                "name": "NovelLover",
+                "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+            }
+        ]
+    },
+
+    {
+        "bookName": "ASKIR - Die komplette Saga 1",
+        "author": "Richard Schwartz",
+        "likes": 2758,
+        "liked": true,
+        "price": 15.75,
+        "publishedYear": 2014,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Halodri",
+                "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+            },
+            {
+                "name": "Bookworm84",
+                "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+            },
+            {
+                "name": "FantasyFanatic",
+                "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+            },
+            {
+                "name": "SciFiGuru",
+                "comment": "Eine fantastische Geschichte mit interessanten Konzepten und Charakteren."
+            },
+            {
+                "name": "NovelLover",
+                "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+            }
+        ]
+    },
+
+    {
+        "bookName": "Die Elfen",
+        "author": "Bernhard Hennen",
+        "likes": 6273,
+        "liked": true,
+        "price": 18.99,
+        "publishedYear": 2004,
+        "genre": "Fantasy",
+        "comments": [
+            {
+                "name": "Halodri",
+                "comment": "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat."
+            },
+            {
+                "name": "Bookworm84",
+                "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+            },
+            {
+                "name": "FantasyFanatic",
+                "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+            },
+            {
+                "name": "SciFiGuru",
+                "comment": "Eine fantastische Geschichte mit interessanten Konzepten und Charakteren."
+            },
+            {
+                "name": "NovelLover",
+                "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+            }
+        ]
+    },
+]
