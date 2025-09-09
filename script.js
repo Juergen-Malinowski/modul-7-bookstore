035a6ec46b063ca01e0d68741b17aa4905d757c3
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-09 19:49:54 +0200

    save discriptions

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let books = [
         "price": 18.95,
         "publishedYear": 1937,
         "genre": "Fantasy",
+        "discription": "In einem Loch im boden, da lebte ein Hobbit... Es war ein schöner Morgen, als ein alter Mann bei Bilbo anklopfte. 'Wir wollen hier keine Abenteuer, vielen Dank!', wimmelte er den ungebetenen Besucher ab...",
         "comments": [
             {
                 "name": "Halodri",
@@ -43,6 +44,7 @@ let books = [
         "price": 17.95,
         "publishedYear": 2007,
         "genre": "Fantasy",
+        "discription": "Die böse Macht Morgoths breitet sich immer weiter nach Beleriand aus. Horden von Orks und der fürchterliche Drache Glaurung bedrohen seine Bewohner... Während Húrin von Morgoth gefangen gehalten wird, nimmt sein tapferer Sohn Túrin den Kampf gegen ds Böse auf ...",
         "comments": [
             {
                 "name": "Halodri",
@@ -76,6 +78,7 @@ let books = [
         "price": 22.95,
         "publishedYear": 1977,
         "genre": "Fantasy",
+        "discription": "Das Silmarillion erklärt die Welt rund um den Herrn der Ringe, die Zeit des Ersten Zeitalters, dem Entstehen der Welt. Woher kamen die Elfen, wie entwickelten sie sich. Ein Muss für jeden Tolkien-Fan. ",
         "comments": [
             {
                 "name": "Halodri",
@@ -109,6 +112,7 @@ let books = [
         "price": 15.75,
         "publishedYear": 2014,
         "genre": "Fantasy",
+        "discription": "Ein verschneiter Gasthof. Ein Krieger mit dunklen Geheimnissen. Eine undurchschaubare Magierin. Und das Tor zu einer gefährlichen Welt, die längst vergessen schien ...",
         "comments": [
             {
                 "name": "Halodri",
@@ -142,6 +146,7 @@ let books = [
         "price": 18.99,
         "publishedYear": 2004,
         "genre": "Fantasy",
+        "discription": "Menschen fürchten sie, wegen ihrer scheinbaren Kaltherzigkeit, Zwerge meiden sie, wegen ihrer Überheblichkeit und Trolle sehen in den sagenumwobenen Wesen eine schmackhafte Beute, die es zu erringen gilt. ",
         "comments": [
             {
                 "name": "Halodri",
@@ -166,3 +171,11 @@ let books = [
         ]
     },
 ]
+
+console.log(books[1]);
+console.log(books[1].comments);
+console.log(books[3].bookName + " von " + books[3].author + " aus " + books[3].publishedYear);
+
+
+
+
