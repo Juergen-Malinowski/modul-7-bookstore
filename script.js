27ed8b6c3a43ef15cce048fba895eb93d1838674
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-10 09:28:03 +0200

    changes

--- script.js
+++ script.js
@@ -179,8 +179,6 @@ console.log(books[1]);
 console.log(books[1].comments);
 console.log(books[3].bookName + " von " + books[3].author + " aus " + books[3].publishedYear);
 
-
-
 for (let index = 0; index < books.length; index++) {
     console.log(books[index].discription);
 }
