3c7050ff45fbce152b0f1c8ffc2dcc20318b99b8
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-11 17:20:02 +0200

    basic Book-List ready

--- script.js
+++ script.js
@@ -33,18 +33,41 @@ function showBooks() {
 function renderBooks(index) {
     // console.log(books[index].bookName);
     return `
-        <h2> ${ books[index].bookName }</h2>
-        <img class="img_grafik" src = "${books[index].bookCover}"  alt = "">
+        <section  class="main_grafik">
+            <h2> ${books[index].bookName}</h2>
+
+            <!-- show: Book-Cover, Like-Heart, Likes ... -->
+
+            <figure class="cover_grafik">
+                <img class="img_grafik" src = "${books[index].bookCover}"  alt = "">
+                <div class="row_likes_grafik">
+                    <img class="heart_img_grafik" src="./assets/logo/heart-thin-icon.png" alt="">
+                    <p class="likes_grafik">${books[index].likes} </p>
+                </div>
+                <br>
+            </figure>
+
+            <!-- // show: Book-Details ... -->
+
+            <div class="position_book_details">
+
+                <p class="price_details">Preis: ${books[index].price} € </p>
+                <p class="book_details">Autor: ${books[index].author} </p>
+                <p class="book_details">Erscheinungsjahr: ${books[index].publishedYear} </p>
+                <p class="book_details">Genre: ${books[index].genre} </p>
+
+            </div>
+        </section>
     `
 }
 
 // function renderMyImg(index) {
 //     // onkeyup and onclick for User-Action
 //     return `
-//         <img onclick="showBigImg(${index}, event)" 
+//         <img onclick="showBigImg(${index}, event)"
 //         onkeyup="showBigImg(${index}, event)"
-//         class="img_size" src="./assets/img/${myImg[index]}" tabindex="0" 
-//         aria-label="./assets/img/${myDiscribtion[index]}" 
+//         class="img_size" src="./assets/img/${myImg[index]}" tabindex="0"
+//         aria-label="./assets/img/${myDiscribtion[index]}"
 //         alt="./assets/img/${myDiscribtion[index]}">
 //         `
 // }
