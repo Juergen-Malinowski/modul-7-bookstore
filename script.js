2a18c35ce9317e61743c74e66fc235520d88148d
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-11 12:17:21 +0200

    start to get dialog-box

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Schreibweise: ID mit Bindestrich, Class mit Unterstrich, JS in CamelCase
 
 // DEFINITION VARIABLE/CONST
-
+const dialogActivity = document.getElementById("book-store");
 
 // test-area
 console.log(books[1]);
@@ -17,6 +17,28 @@ for (let index = 0; index < books.length; index++) {
 }
 
 // End of test-area
+
+
+// DIALOG-Control / open-close
+function openDialog() {
+    dialogActivity.showModal();
+}
+
+// Klären, welche der nachfolgenden Funktionen noch nötig sind
+
+// function protectPopagation(event) {
+//     event.stopPopagation;
+// }
+
+// function closeDialog(event) {
+//     // to check rihgt Keys for close this dialog (WCAG)
+//     if (event.key === " " || event.key === "Enter" || event.type === "click") {
+//     dialogActivity.close();
+//     event.stopPopagation;
+//     } else {
+//         return
+//     }
+// }
 
 
 
